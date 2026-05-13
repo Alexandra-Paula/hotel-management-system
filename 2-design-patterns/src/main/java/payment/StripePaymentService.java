34e6f507a087ce44f978110b6985c66cbc7a6eaa
Alexandra <sandamanea05@gmail.com>
2026-05-13 23:51:11 +0300

    improve payment service abstraction

--- 2-design-patterns/src/main/java/payment/StripePaymentService.java
+++ 2-design-patterns/src/main/java/payment/StripePaymentService.java
@@ -2,7 +2,7 @@ package payment;
 
 public class StripePaymentService {
     public void makeStripePayment(double amount) {
-        System.out.println("Stripe: Processing payment of €" + String.format("%.2f", amount));
-        System.out.println("Stripe: Payment successful!");
+        System.out.println("Processing payment of €" + String.format("%.2f", amount));
+        System.out.println("Payment successful!");
     }
 }
